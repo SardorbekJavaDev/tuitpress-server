2b0dce78d67ce51d1d99e875640922fad0767275
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2023-10-18 15:49:38 +0500

    configuration pom.xml

---
+++ tuitpres/src/main/java/uz/tuit/press/TuitpresApplication.java
@@ -0,0 +1,13 @@
+package uz.tuit.press;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuitpresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuitpresApplication.class, args);
+	}
+
+}
---
+++ tuitpres/src/test/java/uz/tuit/press/TuitpresApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tuit.press;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuitpresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
